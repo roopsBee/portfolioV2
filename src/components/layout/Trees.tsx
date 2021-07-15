7464e45fd0cc45732f272dc630aca24b7ed38541
roopsBee <35971830+roopsBee@users.noreply.github.com>
2021-07-15 20:11:53 +0100

    refactor(component): create new Trees component

---
+++ src/components/layout/Trees.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { mq } from '@/theme/theme'
+import getRandomBetween from '../../helpers/getRandomBetween'
+import Grass from '../Grass'
+import Tree from '../Tree'
+
+interface Props {
+  animate: boolean
+  amount?: number
+  delay?: number
+  grassDelay?: number
+}
+
+const Trees: React.FC<Props> = ({
+  animate,
+  amount = 10,
+  delay = 0,
+  grassDelay = 0,
+}) => {
+  return (
+    <>
+      {new Array(amount).fill(1).map((t, i) => {
+        const bottom = getRandomBetween(5, 15) // tree position from bottom
+        const bottomPercent = ((bottom - 4) / 10) * 100 // map bottom 5-15 to 1-100%
+        const left = `${getRandomBetween(0, 100)}%` // position from left
+        const height = `${((100 - bottomPercent) / 100) * 40}%` // tree size
+        const zIndex = Math.floor((15 / bottom) * 100)
+        const grassPosition = () => `translate(${getRandomBetween(-85, 0)}%)`
+        return (
+          <div key={Math.random()}>
+            <Grass
+              css={{
+                left,
+                bottom: `${bottom + 0.2}%`,
+                zIndex,
+                height: `calc(${height}/4)`,
+                transform: grassPosition(),
+              }}
+              delay={grassDelay + delay + i * 0.2}
+              animate={animate}
+            />
+            <Tree
+              key={Math.random()}
+              css={mq({
+                bottom: `${bottom}%`,
+                left,
+                right: 0,
+                width: 'auto',
+                height,
+                zIndex,
+              })}
+              animate={animate}
+              delay={delay + i * 0.2}
+            />
+            <Grass
+              css={{
+                left,
+                bottom: `${bottom - 1}%`,
+                zIndex,
+                height: `calc(${height}/4)`,
+                transform: grassPosition(),
+              }}
+              animate={animate}
+              delay={grassDelay + delay + i * 0.2}
+            />
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default Trees
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,10 @@ import { motion } from 'framer-motion'
 import { PageProps } from 'gatsby'
 import styled from '@emotion/styled'
 import { mq } from '@/theme/theme'
-import getRandomBetween from '../helpers/getRandomBetween'
-import Grass from '../components/Grass'
+import Trees from '../components/layout/Trees'
 import FrontEndDeveloper from '../components/FrontEndDeveloper'
 import Name from '../components/Name'
 import TypedText from '../components/TypedText'
-import Tree from '../components/Tree'
 
 const Container = styled(motion.div)({
   padding: '20px',
@@ -19,60 +17,12 @@ const Container = styled(motion.div)({
   height: '100vh',
 })
 
-const treeArr = new Array(10).fill(1)
-
 const Home: React.FC<PageProps> = () => {
   const [animate, setAnimate] = useState(true)
 
   const handleTypedTextEnd = async () => {
     setAnimate(true)
   }
-  const trees = treeArr.map((t, i) => {
-    const bottom = getRandomBetween(5, 15)
-    const left = `${getRandomBetween(0, 100)}%`
-    const height = `${(5 / bottom) * 40}%`
-    const zIndex = Math.floor((15 / bottom) * 100)
-    const grassPosition = () => `translate(${getRandomBetween(-85, 0)}%)`
-    return (
-      <div key={Math.random()}>
-        <Grass
-          css={{
-            left,
-            bottom: `${bottom + 0.2}%`,
-            zIndex,
-            height: `calc(${height}/4)`,
-            transform: grassPosition(),
-          }}
-          delay={i * 0.2}
-          animate={animate}
-        />
-        <Tree
-          key={Math.random()}
-          css={mq({
-            bottom: `${bottom}%`,
-            left,
-            right: 0,
-            width: 'auto',
-            height,
-            zIndex,
-          })}
-          animate={animate}
-          delay={i * 0.2}
-        />
-        <Grass
-          css={{
-            left,
-            bottom: `${bottom - 1}%`,
-            zIndex,
-            height: `calc(${height}/4)`,
-            transform: grassPosition(),
-          }}
-          animate={animate}
-          delay={i * 0.2}
-        />
-      </div>
-    )
-  })
 
   return (
     <>
@@ -104,7 +54,7 @@ const Home: React.FC<PageProps> = () => {
           animate={animate}
           delay={2.5}
         />
-        {trees}
+        <Trees delay={1} grassDelay={5} amount={10} animate={animate} />
       </Container>
     </>
   )

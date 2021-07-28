41203df32cb85b6b2b7fa38214493e20137ee9e3
roopsBee <35971830+roopsBee@users.noreply.github.com>
2021-07-28 21:43:05 +0100

    feature:add netlify cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  plugins: [`gatsby-plugin-emotion`, 'gatsby-plugin-loadable-components-ssr'],
+  plugins: [
+    `gatsby-plugin-emotion`,
+    'gatsby-plugin-loadable-components-ssr',
+    'gatsby-plugin-netlify-cms',
+  ],
   flags: {
     // FAST_DEV: true,
     // FAST_REFRESH: true,
